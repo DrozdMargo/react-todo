b0429bf6af227bfbbf96a607dd36a07c1f7199c1
margo <margo@quartsoft.com>
2017-03-14 11:08:24 +0200

    prevent empty form submit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoForm, TodoList} from './components/todo/';
+
+import {addTodo, generateId} from './lib/todoHelpers'
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            todos: [
-                {id: 1, name: 'Learn JSX', isComplete:true},
-                {id: 2, name: 'Build an Awesome App', isComplete:false},
-                {id: 3, name: 'Something else', isComplete:false}
-            ]
-        }
+    state = {
+    todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Something else', isComplete: false}
+    ],
+    currentTodo: ''
+  }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+            const newId = generateId()
+            const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+            const updatedTodos = addTodo(this.state.todos, newTodo)
+            this.setState({
+                todos: updatedTodos,
+                currentTodo: ''
+            })
+    }
+    handleEmptySubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            errorMessage: 'Please supply a todo name'
+        })
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            currentTodo: e.target.value
+        })
     }
   render() {
+        const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>React Todos</h2>
         </div>
+
         <div className="Todo-App">
-            <form>
-                <input type="text"/>
-            </form>
-            <div className="Todo-list">
-                <ul>
-                    {this.state.todos.map(todo =>
-                        <li key={todo.id}>
-                            <input type="checkbox" defaultChecked={todo.isComplete} />{todo.name}
-                        </li>)}
-                </ul>
-            </div>
+            {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
+            <TodoForm handleInputChange={this.handleInputChange}
+                      currentTodo={this.state.currentTodo}
+                      handleSubmit={submitHandler}
+            />
+            <TodoList todos={this.state.todos}/>
+
 
         </div>
       </div>
